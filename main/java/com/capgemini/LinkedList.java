fb7548f8bd64fcd16dc7c51066998d36a8e95ac3
n-ndini7 <hp@RJs-PC>
2020-10-11 10:04:33 +0530

    [nandini] added master

---
+++ main/java/com/capgemini/LinkedList.java
@@ -0,0 +1,9 @@
+package com.capgemini;
+
+//master branch - welcome message
+public class LinkedList {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to introduction to Data Structures!");
+	}
+}
